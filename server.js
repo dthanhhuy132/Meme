f1ea1f292ea40e215eea91cbb2679d8533e0b6d3
HUY <dthanhhuy132@gmail.com>
2021-07-22 13:55:21 +0700

    commit commet color

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('app', './src/index.js')
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.get('/', function (req, res) {
   res.send('Hello Word')
