eef318ee94ae43cbfebc8d76c65c9b9edf511d1f
HUY <dthanhhuy132@gmail.com>
2021-07-22 16:28:25 +0700

    change in server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('app', './src/index.js')
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+
 
 app.get('/', function (req, res) {
   res.send('Hello Word')
 })
+
+app.listen(process.env.PORT || 3000)
