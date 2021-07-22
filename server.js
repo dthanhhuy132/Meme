08f4acc03a43ffd82fde369814471e60ae240edb
HUY <dthanhhuy132@gmail.com>
2021-07-22 13:03:05 +0700

    update listen in server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.set('view engine', 'ejs')
 app.set('app', './src')
 app.listen(process.env.PORT || 3000)
 
-app.listen(3000)
 app.get('/', function (req, res) {
-  res.send('index')
+  res.send('Hello Word')
 })
