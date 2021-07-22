de7eb757139936bcaac18672f163302ac7836b06
HUY <dthanhhuy132@gmail.com>
2021-07-22 13:12:21 +0700

    add src index.js in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.set('view engine', 'ejs')
-app.set('app', './src')
+app.set('app', './src/index.js')
 app.listen(process.env.PORT || 3000)
 
 app.get('/', function (req, res) {
