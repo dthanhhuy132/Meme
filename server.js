5c52613da5c50025d2bc827486972722f6daca5c
HUY <dthanhhuy132@gmail.com>
2021-07-23 21:53:23 +0700

    modify server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express'
-
 const app = express()
+let server = require('http').Server(app);
 
 app.get('/', function (req, res) {
   res.send('Hello Word')
 })
 
 app.listen(process.env.PORT || 3000)
+server.listen(port, function () {
+  console.log("App is running on port " + port);
+});
